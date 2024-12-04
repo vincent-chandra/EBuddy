2162322dbb2033f24d0cd9ff1a4fdf9fd685a8e1
vincent-chandra <vincentchandra1@gmail.com>
2024-12-04 15:14:42 +0700

    initiate project

---
+++ EBuddy/EBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EBuddyApp.swift
+//  EBuddy
+//
+//  Created by Vincent on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
